7cc805f729eee3dbba0ca3b63fba1aebb050dbf1
Igor Antunes Coelho <125463464+igorantunes04@users.noreply.github.com>
2024-06-20 14:11:15 -0300

    alterações e refatorção do projeto

---
+++ template/cap-6/cap-6-5/script_modal.js
@@ -0,0 +1,7 @@
+$('#show-modal').on('click', function () {
+    $('.overlay-6-5, .notice-6-5').show();
+});
+
+$('.overlay-6-5, .close-6-5').on('click', function () {
+    $('.overlay-6-5, .notice-6-5').hide();
+});
