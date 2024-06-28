b646defb5ea8c0457ac60167c0187468a160e7ff
Igor Antunes Coelho <125463464+igorantunes04@users.noreply.github.com>
2024-06-28 15:24:49 -0300

    cap 8.8 finalizado

--- template/cap-7/cap-7-8/exemplos-cap-7-8.js
+++ template/cap-7/cap-7-8/exemplos-cap-7-8.js
@@ -8,3 +8,13 @@ jQuery(function ($) {
         $('html').addClass('no-placeholder');
     }
 });
+
+$('#popup').fadeOut(function () {
+    // o '#popup' já está com opacidade 0;
+});
+    Pode ser escrito assim, utilizando o transitionend em um navegador baseado
+    no WebKit, e uma classe.hidden que altera a opacidade:
+$('#popup').on('webkitTransitionEnd', function () {
+    // A transição do '#popup' foi finalizada.
+})
+$('#popup').addClass('hidden');
